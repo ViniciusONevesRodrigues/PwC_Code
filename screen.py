abbde36b548adf3b65c0e7d34b51667b0c7edcc7
ViniciusONevesRodrigues <vinicius.oliveira.neves.rodrigues@gmail.com>
2024-02-10 18:37:33 -0300

    feat: add test in screen

--- screen.py
+++ screen.py
@@ -1,49 +1,69 @@
+import unittest
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from functions import *
+from tests.test_functions import TestAddressFunctions
+from io import StringIO
 
-class MinhaInterface(QWidget):
+class MyInterface(QWidget):
     def __init__(self):
         super().__init__()
 
         font = QFont()
+        fontTest = QFont()
         font.setPointSize(16)
-        self.setFixedSize(600, 400)
+        fontTest.setPointSize(8)
+
+        self.setFixedSize(800, 600)
 
         self.label = QLabel("Digite o endereço:")
         self.label.setFont(font)
+        self.console_output = QPlainTextEdit(self)
+        self.console_output.setReadOnly(True)
+        self.console_output.setFont(fontTest)
         self.entry = QLineEdit()
         self.button = QPushButton("Separar")
+        self.buttonTest = QPushButton("Testar")
         self.button.setStyleSheet('background-color: #FFA500;')
+        self.buttonTest.setStyleSheet('background-color: #FFA500;')
         self.setStyleSheet('background-color: #ffffff;')
 
         self.groupBox = QGroupBox("Resposta")
         self.groupBox.setFont(font)
-        
         self.labelAddress = QLabel("")
         self.labelAddress.setFont(font)
-        
         groupBoxLayout = QVBoxLayout()
         groupBoxLayout.addWidget(self.labelAddress)
         self.groupBox.setLayout(groupBoxLayout)
 
         self.button.clicked.connect(self.on_button_click)
+        self.buttonTest.clicked.connect(self.on_button_test_click)
+
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.entry)
         layout.addWidget(self.button)
-        layout.addWidget(self.groupBox) 
+        layout.addWidget(self.buttonTest)
+        layout.addWidget(self.groupBox)
+        layout.addWidget(self.console_output)
 
         self.setLayout(layout)
-
         self.setWindowTitle("Desafio_Codigo")
 
     def on_button_click(self):
         address = self.entry.text()
         self.labelAddress.setText(address_redirector(address))
-    
 
-app = QApplication([])
-interface = MinhaInterface()
-interface.show()
-app.exec_()
+    def on_button_test_click(self):
+        test_suite = unittest.TestLoader().loadTestsFromTestCase(TestAddressFunctions)
+        test_runner = unittest.TextTestRunner(stream=StringIO(), verbosity=2)
+        result = test_runner.run(test_suite)
+
+        output_text = result.stream.getvalue()
+        self.console_output.setPlainText(output_text)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    interface = MyInterface()
+    interface.show()
+    app.exec_()
